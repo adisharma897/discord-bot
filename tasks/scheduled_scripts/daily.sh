8f0bda7a0eec8838aeac45b1aa08a3991bcec368
Aditya Sharma <Aditya@Adityas-MacBook-Pro.local>
2023-10-20 17:59:55 +0530

    Fixes in daily sh

--- tasks/scheduled_scripts/daily.sh
+++ tasks/scheduled_scripts/daily.sh
@@ -1,4 +1,1 @@
-python3 --version
-PATH="/home/ubuntu/personal-bot/env/bin:$PATH"
-python3 --version
-python3 /home/ubuntu/personal-bot/intelligent_investment/mean_reversion.py
+python3 intelligent_investment/mean_reversion.py
